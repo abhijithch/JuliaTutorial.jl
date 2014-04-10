d73f85315d6a851c175d06d038d1a2a39b817f35
Abhijith Chandraprabhu <abhijithc@gmail.com>
2014-04-10 15:51:32 +0530

    Updated PrepTest()

--- src/Preparation.jl
+++ src/Preparation.jl
@@ -67,7 +67,7 @@ function PrepTest(path::ASCIIString)
 end
 
 function PrepTest()
-    temp=readdlm("./Test.txt",' ';has_header=false)
+    temp=readdlm("./test/data/Test.txt",' ';has_header=false)
     T=hcat(temp[:,1],temp[:,4])
     return T
 end
